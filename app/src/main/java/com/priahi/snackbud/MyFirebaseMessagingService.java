0b0cedb212b055db88cae7b035ce13336eb24e72
ArnoldYing <arnoldying825@gmail.com>
2020-10-21 11:46:55 -0700

    added push notif code

--- app/src/main/java/com/priahi/snackbud/MyFirebaseMessagingService.java
+++ app/src/main/java/com/priahi/snackbud/MyFirebaseMessagingService.java
@@ -4,11 +4,23 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.firebase.messaging.FirebaseMessagingService;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private static String TAG = "PushNotification";
+    final String url = "http://13.68.137.122:3000/register";
 
     @Override
     public void onNewToken(@NonNull String token) {
@@ -16,6 +28,29 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         Log.d(TAG, "Refreshed token: " + token);
 
         // send the token to our backend
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("userId", token);
+
+        JsonObjectRequest request = new JsonObjectRequest(url, new JSONObject(params),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            VolleyLog.v("Response:%n %s", response.toString(4));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.e("Error: ", error.getMessage());
+            }
+        });
+
+        queue.add(request);
 
     }
 }
