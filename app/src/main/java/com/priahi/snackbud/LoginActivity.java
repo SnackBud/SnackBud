341126526d349c51a2551e25aa26c37c02449fb7
ArnoldYing <arnoldying825@gmail.com>
2020-10-25 19:30:42 -0700

    fixed user post request

--- app/src/main/java/com/priahi/snackbud/LoginActivity.java
+++ app/src/main/java/com/priahi/snackbud/LoginActivity.java
@@ -5,6 +5,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
@@ -19,7 +21,9 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -32,6 +36,9 @@ public class LoginActivity extends AppCompatActivity {
     private int RC_SIGN_IN = 1;
     final static String TAG = "LoginActivity";
 
+    private RequestQueue queue;
+    private String userId;
+    private String username;
     private static final String url = "http://13.68.137.122:3000";
 
     @Override
@@ -94,12 +101,13 @@ public class LoginActivity extends AppCompatActivity {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
 
             // Signed in successfully, show authenticated UI.
+            updateUI(account);
             Intent intent = new Intent(this, PermissionsActivity.class);
             startActivity(intent);
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.w(TAG, "signInResult:failed code = " + e.getStatusCode());
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
             updateUI(null);
         }
     }
@@ -109,43 +117,66 @@ public class LoginActivity extends AppCompatActivity {
             Log.d(TAG, "There is no user signed in");
         }
         else {
-            String personId = account.getId();
-            String personName = account.getDisplayName();
+            userId = account.getId();
+            username = account.getDisplayName();
             Uri personPhoto = account.getPhotoUrl();
 
-            // volley to backend user POST
-            Log.d(TAG, "Sending userId and deviceToken to backend");
-
-            RequestQueue queue = Volley.newRequestQueue(this);
-            HashMap<String, String> params = new HashMap<String, String>();
-            params.put("userId", personId);
-            params.put("deviceToken", MyFirebaseMessagingService.deviceToken);
-            params.put("username", personName);
-
-            JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST,
-                    url + "/user",
-                    new JSONObject(params),
-                    new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            try {
-                                VolleyLog.v("Response:%n %s", response.toString(4));
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    VolleyLog.e("Error: ", error.getMessage());
-                }
-            });
-
-            queue.add(request);
+            sendRegistrationToServer();
 
             Intent intent = new Intent(this, PermissionsActivity.class);
             startActivity(intent);
         }
+    }
+
+    private void sendRegistrationToServer() {
+        // volley to backend user POST
+        Log.d(TAG, "Sending userId and deviceToken to backend");
+        queue = Volley.newRequestQueue(this);
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        HashMap<String, String> params = new HashMap<String, String>();
+                        params.put("userId", userId);
+                        params.put("username", username);
+                        params.put("deviceToken", token);
+
+                        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST,
+                                url + "/user",
+                                new JSONObject(params),
+                                new Response.Listener<JSONObject>() {
+                                    @Override
+                                    public void onResponse(JSONObject response) {
+                                        try {
+                                            VolleyLog.v("Response:%n %s", response.toString(4));
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                VolleyLog.e("Error: ", error.getMessage());
+                            }
+                        });
+
+                        queue.add(request);
+
+                        Log.d(TAG, "token: " + token);
+                    }
+                });
+
+
+
     }
 
 }
