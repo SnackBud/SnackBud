d32fd19d51f1ec47fdc4bbb59a624a872a98f18b
Parsa Riahi <priahi12@gmail.com>
2020-10-26 20:33:04 -0700

    put works although still displays error message

--- app/src/main/java/com/priahi/snackbud/VerifyMeetup.java
+++ app/src/main/java/com/priahi/snackbud/VerifyMeetup.java
@@ -226,11 +226,17 @@ public class VerifyMeetup extends DialogFragment implements AdapterView.OnItemSe
 
     private void putRequest() throws JSONException {
 
-        JSONObject eventRequest = new JSONObject();
-        eventRequest.put("guestId", acct.getId());
-        eventRequest.put("eventId", this.eventId);
-        eventRequest.put("verifyCode", userInputCode);
-
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(requireActivity());
+        if (acct == null) {
+            Log.e(TAG, "error, no google sign in");
+            return;
+        }
+        Log.e(TAG, "about to PUT");
+        JSONObject eventRequest = new JSONObject("{guestId: "+acct.getId()+", eventId: "+this.eventId+", verifyCode: "+userInputCode+"}");
+//        eventRequest.put("guestId", acct.getId());
+//        eventRequest.put("eventId", this.eventId);
+//        eventRequest.put("verifyCode", userInputCode);
+        Log.e(TAG, eventRequest.toString());
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.PUT,
                 url + "/event",
                 eventRequest,
