5cc73d1396d2e214921550114de0e4fbbe57af06
ArnoldYing <arnoldying825@gmail.com>
2020-10-29 20:57:43 -0700

    merge issues

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv/config');
+
+app.use(bodyParser.json());
+
+const router = express.Router()
+
+//import routes
+const userRoute = require('./routes/user.js');
+const eventRoute = require('./routes/event.js');
+app.use('/user', userRoute);
+app.use('/event', eventRoute);
+
+app.get('/', (req, res) => {
+  res.send("home");
+  console.log("home accessed");
+});
+
+// connect to db
+mongoose.connect(process.env.DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+  console.log('connected to db');
+});
+
+//listen on port 3000
+app.listen(3000);
---
+++ backend/models/event.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const maxCode = 999;
+const meetup = mongoose.Schema({
+    eventId: String,
+    hostId: {
+        type: String,
+        required: true
+    },
+    guestIds: [{
+        guestId: {
+            type: String,
+            required: true
+        }
+    }],
+    restId:  {
+        type: String,
+        required: true
+    },
+    restName: {
+        type: String,
+        required: true
+    },
+    timeOfMeet: {
+        type: Number,
+        required: true
+    },
+    timeOfCreation: {
+        type: Number,
+        default: new Date().getTime()
+    },
+    isVerified:  {
+        type: Boolean,
+        default: false
+    },
+    verifyCode:  {
+        type: String,
+        default: Math.floor(Math.random() * Math.floor(maxCode))
+    },
+})
+
+module.exports = mongoose.model('meetup', meetup);
