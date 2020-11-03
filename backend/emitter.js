e41d50c1dc5b09a7074d46db685b051310b8156a
ArnoldYing <arnoldying825@gmail.com>
2020-11-02 17:11:23 -0800

    debug

--- backend/emitter.js
+++ backend/emitter.js
@@ -3,35 +3,35 @@ const helpers = require('./helper');
 const pushNotify = new EventEmitter();
 
 // First listener for new meetup
-pushNotify.on('newMeetup', async function firstListener(event) {
+pushNotify.on('newMeetup', function firstListener(event) {
     helper = new helpers();
     helper.printToConsole(event);
     helper.notifyNewMeetup(event);
 });
 
 // First listener for new meetup
-pushNotify.on('verifyMeetup', async function firstListener(event, guest) {
+pushNotify.on('verifyMeetup', function firstListener(event, guest) {
     helper = new helpers();
     helper.printToConsole(event);
     helper.notifyVerifyMeetup(event, guest);
 });
 
 // First listener for new meetup
-pushNotify.on('noVerifyMeetup', async function firstListener(guest) {
+pushNotify.on('noVerifyMeetup', function firstListener(guest) {
     helper = new helpers();
     helper.printToConsole(guest);
     helper.notifyNoVerifyMeetup(guest);
 });
 
 //listener for guests when the host unlocks the verification code
-pushNotify.on('enterCode', async function firstListener(event) {
+pushNotify.on('enterCode', function firstListener(event) {
     helper = new helpers();
     helper.printToConsole(event);
     helper.notifyEnterCode(event);
 });
 
 //listener for guests when the host unlocks the verification code
-pushNotify.on('contactTrace', async function firstListener(sickUser, atRiskUser, event) {
+pushNotify.on('contactTrace', function firstListener(sickUser, atRiskUser, event) {
     helper = new helpers();
     helper.printToConsole(event);
     helper.notifyHelper(atRiskUser, sickUser.username + ' has COVID-19 symptoms! You were in contact with them in the last two weeks!',
@@ -39,7 +39,7 @@ pushNotify.on('contactTrace', async function firstListener(sickUser, atRiskUser,
 });
 
 //listener for guests when the host unlocks the verification code
-pushNotify.on('finishContactTrace', async function firstListener(sickUser, numNotified) {
+pushNotify.on('finishContactTrace', function firstListener(sickUser, numNotified) {
     helper = new helpers();
     helper.printToConsole(sickUser);
     if (numNotified > 0) {
