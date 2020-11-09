66fc5d50df18667ee67883d8cdddf6db645aa354
sanjeevk1999 <sanjeevk1999@gmail.com>
2020-11-09 20:44:24 +0530

    calendar fix codacy

--- app/src/main/java/com/priahi/snackbud/MeetingFragment.java
+++ app/src/main/java/com/priahi/snackbud/MeetingFragment.java
@@ -1,14 +1,13 @@
 package com.priahi.snackbud;
 
 import android.app.DatePickerDialog;
-import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.*;
 
+import android.widget.*;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -275,7 +274,7 @@ public class MeetingFragment extends Fragment implements View.OnClickListener, A
             datePickerDialog.getDatePicker().setMinDate(c.getTimeInMillis());
             c.add(Calendar.MONTH, +1);
             long oneMonthAhead = c.getTimeInMillis();
-            //datePickerDialog.getDatePicker().setMaxDate(oneMonthAhead);
+            datePickerDialog.getDatePicker().setMaxDate(oneMonthAhead);
             datePickerDialog.show();
             //TODO: make this timezone invariant
         } else if (v.equals(btnTimePicker)) {
