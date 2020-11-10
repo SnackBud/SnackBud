15c28ed2a885fbdbd4b37d4c8d648f3813e14fdd
ArnoldYing <arnoldying825@gmail.com>
2020-11-09 18:59:49 -0800

    codacy fix test

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-require('dotenv/config');
-const winston = require('winston')
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv/config");
+const winston = require("winston")
 const consoleTransport = new winston.transports.Console()
 const myWinstonOptions = {
   transports: [consoleTransport]
@@ -28,21 +28,21 @@ app.use(bodyParser.json());
 // const router = express.Router();
 
 // import routes
-const userRoute = require('./routes/user.js');
-const eventRoute = require('./routes/event.js');
+const userRoute = require("./routes/user.js");
+const eventRoute = require("./routes/event.js");
 
-app.use('/user', userRoute);
-app.use('/event', eventRoute);
+app.use("/user", userRoute);
+app.use("/event", eventRoute);
 
-app.get('/', (req, res) => {
-  res.send('home');
+app.get("/", (req, res) => {
+  res.send("home");
   // console.log("home accessed");
 });
 
 // connect to db
 mongoose.connect(process.env.DB_CONNECTION,
   { useNewUrlParser: true, useUnifiedTopology: true }, () => {
-  // console.log('connected to db');
+  // console.log("connected to db");
   });
 
 // listen on port 3000
