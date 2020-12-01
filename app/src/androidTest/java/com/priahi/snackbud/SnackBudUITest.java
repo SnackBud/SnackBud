16ab46008d2f0290f0bc29e22cc322666a18c7ab
sanjeev <sanjeevk1999@gmail.com>
2020-12-02 01:46:56 +0530

    travis video5 commit

--- app/src/androidTest/java/com/priahi/snackbud/SnackBudUITest.java
+++ app/src/androidTest/java/com/priahi/snackbud/SnackBudUITest.java
@@ -19,8 +19,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-
-
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
