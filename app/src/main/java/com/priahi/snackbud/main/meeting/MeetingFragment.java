8e114284f239917e20cc049a270359ff8a105f4f
sanjeev <sanjeevk1999@gmail.com>
2020-12-02 00:36:38 +0530

    fix release build

--- app/src/main/java/com/priahi/snackbud/main/meeting/MeetingFragment.java
+++ app/src/main/java/com/priahi/snackbud/main/meeting/MeetingFragment.java
@@ -238,7 +238,7 @@ public class MeetingFragment extends Fragment implements View.OnClickListener {
                         e.printStackTrace();
                     }
                 }, error -> {
-            Log.d(TAG, "Failed with error msg:\t" + error.getMessage());
+            // Log.d(TAG, "Failed with error msg:\t" + error.getMessage());
             Log.d(TAG, "Error StackTrace: \t" + Arrays.toString(error.getStackTrace()));
             // edited here
             try {
